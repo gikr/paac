b2e52da2896c5c4ab13864361142294edb9079b0
gikr <gaini.ikram@gmail.com>
2018-03-25 00:12:11 +0300

    Update Tlab_emulator.py

--- emulators/T_labyrinth/Tlab_emulator.py
+++ emulators/T_labyrinth/Tlab_emulator.py
@@ -1,4 +1,4 @@
-from ..env_T_2 import make_game, T_lab_observation
+from ..env_T_2 import make_game, T_lab_observation, T_lab_actions
 
 from ..environment import BaseEnvironment
 
@@ -9,8 +9,9 @@ import numpy as np
 class TLabyrinthEmulator(BaseEnvironment):
     def __init__(self, actor_id, args):
         self.game = make_game(True, None)
-        self.legal_actions = set([0,1,2,3,4])
-        self.noop = 'pass'
+        self.legal_actions = T_lab_actions().shape
+        #print(self.legal_actions)
+        #self.noop = 'pass'
         self.id = actor_id
         
         self.game = make_game(True, None)
@@ -34,17 +35,21 @@ class TLabyrinthEmulator(BaseEnvironment):
         Performs the given action.
         Returns the next state, reward, and terminal signal
         """
+        act = [i for i, x in enumerate(action) if x]
         
         if not self.game.game_over:
-            observation, reward, discount = self.game.play(action)
-        return observation, reward, discount
+            obs, reward, discount = self.game.play(act)
+        termination = 1-discount
+        
+        observation = T_lab_observation(obs)
+        return observation, reward, termination
 
 
     def get_legal_actions(self):
         return self.legal_actions
 
     def get_noop(self):
-        return self.noop
+        return self.legal_actions[0]
 
     def on_new_frame(self, frame):
         pass
